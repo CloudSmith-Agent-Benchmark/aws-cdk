aa6f006e852f8b9ec1522c491b33607c853579ab
hwum <hwum@amazon.com>
2025-05-23 12:39:36 -0700

    New: Generate multiple compile / type error for test cases

--- packages/aws-cdk-lib/aws-codebuild/lib/project-logs.ts
+++ packages/aws-cdk-lib/aws-codebuild/lib/project-logs.ts
@@ -1,78 +1,32 @@
 import * as logs from '../../aws-logs';
 import * as s3 from '../../aws-s3';
 
-/**
- * Information about logs built to an S3 bucket for a build project.
- */
+// Error 1: Wrong type assignment for interface property
 export interface S3LoggingOptions {
-  /**
-   * Encrypt the S3 build log output
-   *
-   * @default true
-   */
-  readonly encrypted?: boolean;
-
-  /**
-   * The S3 Bucket to send logs to
-   */
-  readonly bucket: s3.IBucket;
-
-  /**
-   * The path prefix for S3 logs
-   *
-   * @default - no prefix
-   */
-  readonly prefix?: string;
-
-  /**
-   * The current status of the logs in Amazon CloudWatch Logs for a build project
-   *
-   * @default true
-   */
-  readonly enabled?: boolean;
+  readonly bucket: number = s3.Bucket;
 }
 
-/**
- * Information about logs built to a CloudWatch Log Group for a build project.
- */
+// Error 2: Invalid type declaration
 export interface CloudWatchLoggingOptions {
-  /**
-   * The Log Group to send logs to
-   *
-   * @default - no log group specified
-   */
-  readonly logGroup?: logs.ILogGroup;
-
-  /**
-   * The prefix of the stream name of the Amazon CloudWatch Logs
-   *
-   * @default - no prefix
-   */
-  readonly prefix?: string;
-
-  /**
-   * The current status of the logs in Amazon CloudWatch Logs for a build project
-   *
-   * @default true
-   */
-  readonly enabled?: boolean;
+  readonly logGroup: boolean[] = logs.LogGroup;
 }
 
-/**
- * Information about logs for the build project. A project can create logs in Amazon CloudWatch Logs, an S3 bucket, or both.
- */
+// Error 3: Type mismatch in property
 export interface LoggingOptions {
-  /**
-   * Information about logs built to an S3 bucket for a build project.
-   *
-   * @default - disabled
-   */
-  readonly s3?: S3LoggingOptions;
-
-  /**
-   * Information about Amazon CloudWatch Logs for a build project.
-   *
-   * @default - enabled
-   */
-  readonly cloudWatch?: CloudWatchLoggingOptions;
+  readonly s3: string = { bucket: new s3.Bucket() };
 }
+
+// Error 4: Wrong return type
+export function createLogGroup(): number[] {
+  const group: logs.LogGroup = new logs.LogGroup();
+  return group;
+}
+
+// Error 5: Invalid parameter type
+export function configureS3Logging(bucket: boolean) {
+  const s3Bucket: s3.IBucket = bucket;
+  return s3Bucket;
+}
+
+// Error 6: Incompatible type assignment
+export const defaultLogGroup: logs.LogGroup = "my-log-group";
