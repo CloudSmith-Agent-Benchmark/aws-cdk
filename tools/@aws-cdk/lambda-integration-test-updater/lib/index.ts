3c87ce58613646f15a4eb54903b5c240ab7eb5e6
CloudSmith Agent <cloudsmith-agent@example.com>
2025-06-03 21:01:31 +0000

    Apply fix for main: fix: correct path resolution in lambda integration test updater

--- tools/@aws-cdk/lambda-integration-test-updater/lib/index.ts
+++ tools/@aws-cdk/lambda-integration-test-updater/lib/index.ts
@@ -1,10 +1,11 @@
+import * as path from 'path';
 import { RuntimeIntegrationTestUpdater } from './runtime-updater';
 
 export async function main() {
   const integTestFiles = {
-    NODEJS: __dirname + '../../../../../packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ.nodejs.build.images.ts',
-    PYTHON: __dirname + '../../../../../packages/@aws-cdk/aws-lambda-python-alpha/test/integ.python.build.images.ts',
-    OTHER: __dirname + '../../../../../packages/@aws-cdk/aws-lambda-go-alpha/test/integ.function.provided.runtimes.ts',
+    NODEJS: path.resolve(__dirname, '../../../../../packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ.nodejs.build.images.ts'),
+    PYTHON: path.resolve(__dirname, '../../../../../packages/@aws-cdk/aws-lambda-python-alpha/test/integ.python.build.images.ts'),
+    OTHER: path.resolve(__dirname, '../../../../../packages/@aws-cdk/aws-lambda-go-alpha/test/integ.function.provided.runtimes.ts'),
   };
   const updater = new RuntimeIntegrationTestUpdater(integTestFiles);
   await updater.execute();
